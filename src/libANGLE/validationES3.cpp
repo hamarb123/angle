31af02902381aea9e7da281dd40411daaf48e916
Hamish Arblaster <hamarb123@gmail.com>
2022-05-30 08:54:48 +1000

    Revert "[M101] Add error check on resuming XFB with deleted buffer."
    This reverts commit e37380e62a427cbb7172b6c17f8752ab96abf356.

--- src/libANGLE/validationES3.cpp
+++ src/libANGLE/validationES3.cpp
@@ -4291,13 +4291,6 @@ bool ValidateResumeTransformFeedback(const Context *context, angle::EntryPoint e
         return false;
     }
 
-    if (!ValidateProgramExecutableXFBBuffersPresent(context,
-                                                    context->getState().getProgramExecutable()))
-    {
-        context->validationError(entryPoint, GL_INVALID_OPERATION, kTransformFeedbackBufferMissing);
-        return false;
-    }
-
     return true;
 }
 
